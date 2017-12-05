c539a6fc236c2bc46cb6ba16caeaec259c241fe6
AntoineO <antoi@DESKTOP-L9H1MBB>
2017-12-05 15:25:25 -0500

    work plz

--- ProjetSFML5/ProjetSFML5/Terminal.h
+++ ProjetSFML5/ProjetSFML5/Terminal.h
@@ -1,12 +1,19 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include "textbox.h"
+#include <string.h>
 class Terminal
 {
 
 public:
 	Terminal();
 	int run();
+	int call669();
+	int callLotoMax();
+	int callPoutine();
+	int callUber();
+	int call007();
+	int Validate(std::string billet);
 private:
 	const int LARGEUR = 1280;
 	const int HAUTEUR = 720;
